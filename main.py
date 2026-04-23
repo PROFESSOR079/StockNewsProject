7b9ea51ad858cf817f23e0e95a83e3c74030ce79
Gugalomi=geo <iva290386@gmail.com>
2026-04-23 17:11:14 +0400

    Step 1 complete: Fetch stock data and calculate percentage difference

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from sys import float_repr_style
+
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -11,5 +14,38 @@ STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+
+parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=parameters)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+
+data_list = [value for (key, value) in data.items()]
+
+yesterday_data = data_list[0]
+yesterday_closing_stock_price = float(yesterday_data["4. close"])
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_stock_price = float(day_before_yesterday_data["4. close"])
+
+difference = abs(yesterday_closing_stock_price - day_before_yesterday_closing_stock_price)
+diff_percent = (difference / day_before_yesterday_closing_stock_price) * 100
+
+if diff_percent > 5:
+    print("Get News!")
+
+
+
+
+
+
+
+
 
 
